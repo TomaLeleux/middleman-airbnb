79e325b99fb3b2fe4a297e0c051df91ca062ae5c
Thomas Leleux <leleux.thomas@gmail.com>
2018-05-14 16:50:28 +0200

    First step for third exercise

--- config.rb
+++ config.rb
@@ -20,3 +20,9 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each_key do |name|
+  proxy "/flats/#{name.to_s}.html", "/flats/show.html",
+  locals: { owner: name.to_s },
+  ignore: true
+end
